4b9399b99af5cee0889521abb464c55e821c0066
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2020-01-24 16:53:16 -0800

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var config = {
             image: 'image/12-10-2019.jpeg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [-122.420, 37.772],
+                center: [-122.506, 37.775],
                 zoom: 11.45,
                 pitch: 0,
                 bearing: 0
